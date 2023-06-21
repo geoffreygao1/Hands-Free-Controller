d3c786f3d991c97ddd3ced43cd7448012c5653aa
Geoffrey Gao <geoffreygao1@gmail.com>
2023-06-21 10:25:13 -0700

    Initialized Webcam

--- handsfreecontroller/src/Components/App.js
+++ handsfreecontroller/src/Components/App.js
@@ -1,15 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Header from "./Header";
+import Webcam from "react-webcam";
+import * as faceapi from "face-api.js"
 
 
 
 function App() {
+  //Setup references for on-screen components
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+
   return (
     <React.Fragment>
       <Header />
+      <div className="App">
+        <Webcam ref={webcamRef} style={
+          {
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480
+          }
+        }
+        />
+        <canvas ref={canvasRef} style={
+          {
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480
+          }
+        }
+        />
+      </div>
     </React.Fragment>
+
   );
 }
 

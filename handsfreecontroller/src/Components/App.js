aa63ab3a3d2e6cd6cad574a4c8ec5c470ed2112b
Geoffrey Gao <geoffreygao1@gmail.com>
2023-06-21 10:54:42 -0700

    Initialize Video and face landmark detction

--- handsfreecontroller/src/Components/App.js
+++ handsfreecontroller/src/Components/App.js
@@ -1,50 +1,101 @@
-import './App.css';
-import React, { useRef, useEffect } from "react";
-import Header from "./Header";
+import * as faceapi from 'face-api.js';
+import React from 'react';
 import Webcam from "react-webcam";
-import * as faceapi from "face-api.js"
-
-
+import Header from './Header';
 
 function App() {
-  //Setup references for on-screen components
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
 
+  //State for model loaded and capture video
+  const [modelsLoaded, setModelsLoaded] = React.useState(false);
+  const [captureVideo, setCaptureVideo] = React.useState(false);
+
+  //Establish references for webcam and canvas
+  const videoRef = React.useRef();
+  const videoHeight = 240;
+  const videoWidth = 320;
+  const canvasRef = React.useRef();
+
+  //Load Models
+  React.useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+      ]).then(setModelsLoaded(true));
+    }
+    loadModels();
+    startVideo();
+  }, []);
+
+  //Handle initiate Webcam
+  const startVideo = () => {
+    setCaptureVideo(true);
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        setTimeout(function () {
+          video.play();
+        }, 100);
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  }
+
+  const handleVideoOnPlay = () => {
+    setInterval(async () => {
+      if (canvasRef && canvasRef.current) {
+        //Create display on page to match video dimensions
+        canvasRef.current.innerHTML = faceapi.createCanvas(videoRef);
+        const displaySize = {
+          width: videoWidth,
+          height: videoHeight
+        }
+
+        faceapi.matchDimensions(canvasRef.current, displaySize);
+
+        //initiate detections
+        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+        //Draw on canvas
+        canvasRef && canvasRef.current && canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
+        canvasRef && canvasRef.current && faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+      }
+    }, 100)
+  }
+
+  //Handle stop webcam
+  const closeWebcam = () => {
+    videoRef.current.pause();
+    videoRef.current.srcObject.getTracks()[0].stop();
+    setCaptureVideo(false);
+  }
 
   return (
     <React.Fragment>
       <Header />
-      <div className="App">
-        <Webcam ref={webcamRef} style={
-          {
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480
-          }
+      < div >
+        {
+          captureVideo ?
+            modelsLoaded ?
+              <div>
+                <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+                  <video ref={videoRef} height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} style={{ borderRadius: '10px' }} />
+                  <canvas ref={canvasRef} style={{ position: 'absolute' }} />
+                </div>
+              </div>
+              :
+              <div>loading...</div>
+            :
+            <>
+            </>
         }
-        />
-        <canvas ref={canvasRef} style={
-          {
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480
-          }
-        }
-        />
-      </div>
+      </div >
     </React.Fragment>
 
   );
